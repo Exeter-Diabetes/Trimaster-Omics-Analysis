5b2eb78ee2b2a4bd3c3e1c2bd06743ef20b977bd
Pedro Cardoso <104718776+PM-Cardoso@users.noreply.github.com>
2025-07-29 15:31:31 +0100

    Removing leftover code that is not needed.

--- 02.PCA.R
+++ 02.PCA.R
@@ -83,33 +83,3 @@ pca_variable <- factoextra::get_pca_var(pca_analysis)
 save(pca_variable, file = "Data/pca_variable.Rdata")
 
 
-
-
-# # Fit regression for specific benefit
-# ## With PCA
-# lm(benefit ~ prehba1c + PCA1 + PCA2 + PCA3+ PCA4 + PCA5 + PCA6 + PCA7 + PCA8 + PCA9 + PCA10, data = data_analysis %>% left_join(data %>% select(study_id, prehba1c)) %>%left_join(pca_individuals))
-# 
-# ## With PCA and BMI
-# lm(benefit ~ prehba1c + prebmi + preegfr + PCA1 + PCA2 + PCA3+ PCA4 + PCA5 + PCA6 + PCA7 + PCA8 + PCA9 + PCA10, data = data_analysis %>% left_join(data %>% select(study_id, prehba1c, prebmi, preegfr)) %>%left_join(pca_individuals))
-# 
-# ## do LASSO for regression to reduce components to 0 (check if it discards anything)
-# library(glmnet)
-# x = data_analysis %>% left_join(data %>% select(study_id, prehba1c, prebmi, preegfr)) %>%left_join(pca_individuals) %>%
-#   select(-c("benefit", "study_id", variables_proteomics)) %>%
-#   as.matrix()
-# y = data_analysis %>% left_join(data %>% select(study_id, prehba1c, prebmi, preegfr)) %>%left_join(pca_individuals) %>%
-#   select(c("benefit")) %>%
-#   unlist()
-# cv_model <- cv.glmnet(x, y, alpha = 1)
-# best_model <- glmnet(x, y, alpha = 1, lambda = cv_model$lambda.min)
-# 
-# # Check correlations for each PCA
-# pca_variable$cor[,7] %>% View()
-# pca_variable$cor[,8] %>% View()
-# 
-# # Check highest (absolute) correlation between var and PCA
-# cor_highest <- pca_variable$cor[order(abs(pca_variable$cor[,8]), decreasing = TRUE)[1:30],8]
-
-
-
-
